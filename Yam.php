88d50a110dcbe319318e3cd891e2772a7b49b451
nicolaszanforlini <nicolaszanforlini2@hotmail.fr>
2022-01-07 10:53:02 +0100

    Initialisation des Fichiers à développer
    2 fichiers :
    -app.php
    -yam.php

---
+++ Yam.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Fichier de la class Yam.
+ */
+
+namespace App;
+
+/**
+ * # class Yam
+ *  
+ * @author Nicolas Zanforlini <nicolas@nicolas.com>
+ * 
+ */
+class Yam
+{
+
+}
---
+++ app.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+*Créez une classe qui permet de lancer une simulatio d'un lancer de 3 dés à 6 faces, 
+*et de calculer respectivement pour le jeu de Yam les probabilités d'obtenir :
+*Un Yam.
+*Un carré.
+*Un brelan de 1.
+*Faites 4 méthodes respectivement pour simuler un lancer de 3 dés à 6 faces, 
+*une méthode pour calculer les Yam, un carré et un brelan. Créez deux branches dev et main. 
+*Chaque commit pour être parfaitement écrit en respectant les conventions du cours. 
+*Produisez la documentation PHPDoc de la classe correctement identée.
+*/
+
+namespace App;
+
+/**
+ * Fichier de lancement de la simulation.
+ */
+
+
+
+
+
