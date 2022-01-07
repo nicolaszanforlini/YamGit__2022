55811a5127c275af9c1f0b68f1be58e3058c4d1a
nicolaszanforlini <nicolaszanforlini2@hotmail.fr>
2022-01-07 11:10:11 +0100

    Attributs of class Yam and Method to generate a dice roll
    création des attributs de la classe Yam et de la méthode qui génère un
    tableau de lancé de dés.

--- Yam.php
+++ Yam.php
@@ -11,8 +11,31 @@ namespace App;
  *  
  * @author Nicolas Zanforlini <nicolas@nicolas.com>
  * 
+ * 
  */
 class Yam
 {
+    private array $tabFaceDe = [1,2,3,4,5,6];  // faces du dé
+    private array $generateTabDes;             // tableau des lancés de dés
+    private array $tabOccurenceDes;            // tableau des occurences du tableau des lancés de dés
+    private array $resultFinal = ["brelan" => 0, "carre" => 0, "yam" => 0]; // résultat final
 
+    // génère un tableau de dés lancés
+    public function generateDes( int $nb ) : array
+    {
+        $j = 0;
+        while($j<$nb)
+        {
+            $i = 0;
+            $de = [];
+            while($i<5)
+            {
+                $de [] = rand(1, 6);
+                $i++;
+            }
+            $this->generateTabDes [] = $de;
+            $j++;
+        }
+        return $this->generateTabDes;
+    }
 }
